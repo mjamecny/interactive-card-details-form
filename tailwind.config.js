3e8647717705a0c0732eca13cfd2195a2fd6732d
mjamecny <jamecny.m@tutanota.com>
2023-11-27 13:50:02 +0100

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ export default {
     fontFamily: {
       sans: ["'Space Grotesk'", "sans-serif"],
     },
+    colors: {
+      red: "hsl(0, 100%, 66%)",
+      white: "hsl(0, 0%, 100%)",
+      lightGrayishViolet: "hsl(270, 3%, 87%)",
+      darkGrayishViolet: "hsl(279, 6%, 55%)",
+      veryDarkViolet: "hsl(278, 68%, 11%)",
+      purple: "hsl(249, 99%, 64%)",
+      magenta: "hsl(278, 94%, 30%)",
+    },
   },
   plugins: [],
 }
